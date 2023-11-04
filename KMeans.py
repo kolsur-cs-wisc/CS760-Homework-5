c88fe6e81b3a5fff88f908b274713cc18f00b644
Aditya Kolsur <kolsur@wisc.edu>
2023-11-04 14:50:25 -0500

    Implement KMeans

--- KMeans.py
+++ KMeans.py
@@ -1,20 +1,30 @@
 import numpy as np
 
 class KMeans():
-    def __init__(self, k):
+    def __init__(self, k = 3):
         self.k = k
         self.max_iter = 500
 
-    def euclidean(self, point, data):
-        return np.sqrt(np.sum((point - data)**2, axis=1))
+    def euclidean(self, point, centroids):
+        return np.sqrt(np.sum((point - centroids)**2, axis=1))
     
     def fit(self, X_train):
-        min_, max_ = np.min(X_train, axis=0), np.max(X_train, axis=0)
-        self.centroids = [np.uniform(min_, max_) for _ in range(self.n_clusters)]
+        self.X = X_train
+        index = np.random.choice(X_train.shape[0])
+        self.centroids = [X_train[index]]
+        for _ in range(self.k-1):
+            dists = np.sum([self.euclidean(centroid, X_train) for centroid in self.centroids], axis=0)
+            dists /= np.sum(dists)
+            new_centroid_idx, = np.random.choice(range(len(X_train)), size=1, p=dists)
+            self.centroids += [X_train[new_centroid_idx]]
+
+        # min_, max_ = np.min(X_train, axis=0), np.max(X_train, axis=0)
+        # self.centroids = [np.random.uniform(min_, max_) for _ in range(self.k)]
+
         iteration = 0
         prev_centroids = None
-        while np.not_equal(self.centroids, prev_centroids).any() and iteration < self.max_iter:
-            sorted_points = [[] for _ in range(self.n_clusters)]
+        while np.not_equal(self.centroids, prev_centroids).any():
+            sorted_points = [[] for _ in range(self.k)]
             for x in X_train:
                 dists = self.euclidean(x, self.centroids)
                 centroid_idx = np.argmin(dists)
@@ -25,3 +35,19 @@ class KMeans():
                 if np.isnan(centroid).any():
                     self.centroids[i] = prev_centroids[i]
             iteration += 1
+        self.clusters = sorted_points
+        return self.centroids, self.clusters
+    
+    def objective(self):
+        clustering_objective = 0 
+        for idx in range(self.k):
+            centroid = self.centroids[idx]
+            cluster = self.clusters[idx]
+            for X in cluster:
+                clustering_objective += np.sum((X - centroid)**2)
+
+        return clustering_objective
+    
+    def accuracy(self):
+        pass
+    
--- Main_KMeans.py
+++ Main_KMeans.py
@@ -1,19 +1,28 @@
 import numpy as np
+from KMeans import KMeans
 
-def main():
+def generate_datasets():
     sigmas = [0.5, 1, 2, 4, 8]
+    labels = ['a', 'b', 'c']
     means = [[-1, -1], [1, -1], [0, 1]]
     covariances = [[[2, 0.5],[0.5, 1]],[[1, -0.5],[-0.5, 2]], [[1, 0],[0, 2]]]
 
     for sigma in sigmas:
-        f = open(f'dataset_{sigma}.txt','a')
         for i in range(3):
             mean = np.array(means[i])
             covariance = np.array(covariances[i]) * sigma
             curr_sample = np.random.multivariate_normal(mean, covariance, 100)
+            f = open(f'dataset_{sigma}_labelled.txt', 'a')
             np.savetxt(f, curr_sample, delimiter = ' ')
+            f.close()
 
-    pass
+def main():
+    generate_datasets()
+    X_train = np.loadtxt(f'dataset_0.5.txt', delimiter=' ', dtype=float)
+    kmeans_model = KMeans()
+    centroids, clusters = kmeans_model.fit(X_train)
+    objective = kmeans_model.objective()
+    print(f'Cluster Centers = {centroids}, Clustering Objective = {objective}')
 
 if __name__ == "__main__":
     main()
