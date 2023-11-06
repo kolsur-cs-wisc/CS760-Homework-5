6c6fe70e436b1513423b03fcc0427b6a4fe7b300
Aditya Kolsur <kolsur@wisc.edu>
2023-11-05 19:30:57 -0600

    Implement DRO

--- DRO.py
+++ DRO.py
@@ -1,5 +1,22 @@
 import numpy as np
 
 class DRO():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, X, d) -> None:
+        self.X = X
+        self.D = X.shape[1]
+        self.d = d
+
+    def transform(self):
+        self.b = np.mean(self.X, axis = 0)
+        X_center = self.X - self.b
+        U, S, VT = np.linalg.svd(X_center)
+        self.A = np.reshape(VT[:self.d], (self.D, self.d))
+        self.Z = np.matmul(X_center, self.A)
+        return self.Z, self.reconstruct()
+    
+    def reconstruct(self):
+        self.X_recons = np.matmul(self.Z, self.A.transpose()) + self.b
+        return self.X_recons
+
+    def reconstruction_error(self):
+        return np.sum((self.X - self.reconstruct())**2)/len(self.X)
--- Main_DRO.py
+++ Main_DRO.py
@@ -7,24 +7,17 @@ def plotData(X, X_recons):
     plt.scatter(X_recons[:, 0], X_recons[:, 1], c = 'red', marker = 'x')
     plt.xlabel('Dimension 1')
     plt.ylabel('Dimension 2')
-    plt.title(f'{type} PCA')
+    plt.title('DRO Method')
     plt.savefig(f'output/DRO_data2D.png')
     plt.show()
 
 def main():
     X = np.loadtxt('data/data2D.csv', delimiter=',')
-    
-    pca_model = DRO(X, 1)
-    X_pca, X_recons = pca_model.transform()
-    plotData(X, X_recons)
 
-    pca_model = DRO(X, 1, 'demean')
-    X_pca, X_recons = pca_model.transform()
+    dro_model = DRO(X, 1)
+    X_pca, X_recons = dro_model.transform()
     plotData(X, X_recons)
-
-    pca_model = DRO(X, 1, 'normalize')
-    X_pca, X_recons = pca_model.transform()
-    plotData(X, X_recons)
+    np.savetxt('output/DRO Results.txt', [f'DRO 2D Dataset Reconstruction Error = {dro_model.reconstruction_error()}'], fmt = '%s')
 
 if __name__ == '__main__':
     main()
