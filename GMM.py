530697a3b04e53d112591df917e39ac4e3c01ecc
Aditya Kolsur <kolsur@wisc.edu>
2023-11-08 10:24:32 -0600

    Add GMM Results

--- GMM.py
+++ GMM.py
@@ -3,40 +3,31 @@ from scipy import rand
 from scipy.stats import multivariate_normal
 
 class GMM():
-    def __init__(self, k=3, dim=2, init_mu=None, init_sigma=None, init_pi=None):
+    def __init__(self, X, k=3, dim=2):
         self.k = k
-        self.dim = dim
-        if(init_mu is None):
-            init_mu = rand(k, dim)*1 - 0.5
-        self.mu = init_mu
-        if(init_sigma is None):
-            init_sigma = np.zeros((k, dim, dim))
-            for i in range(k):
-                init_sigma[i] = np.eye(dim)
+        self.mu = rand(k, dim)*1 - 0.5
+        init_sigma = np.zeros((k, dim, dim))
+        for i in range(k): init_sigma[i] = np.eye(dim)
         self.sigma = init_sigma
-        if(init_pi is None):
-            init_pi = np.ones(self.k)/self.k
-        self.pi = init_pi
-    
-    def init_em(self, X):
-        self.data = X
-        self.num_points = X.shape[0]
-        self.z = np.zeros((self.num_points, self.k))
+        self.pi = np.ones(self.k)/self.k
+        self.X = X
+        self.n = len(X)
+        self.z = np.zeros((self.n, self.k))
     
     def e_step(self):
         for i in range(self.k):
-            self.z[:, i] = self.pi[i] * multivariate_normal.pdf(self.data, mean=self.mu[i], cov=self.sigma[i])
+            self.z[:, i] = self.pi[i] * multivariate_normal.pdf(self.X, mean=self.mu[i], cov=self.sigma[i])
         self.z /= self.z.sum(axis=1, keepdims=True)
     
     def m_step(self):
         sum_z = self.z.sum(axis=0)
-        self.pi = sum_z / self.num_points
-        self.mu = np.matmul(self.z.T, self.data)
+        self.pi = sum_z / self.n
+        self.mu = np.matmul(self.z.T, self.X)
         self.mu /= sum_z[:, None]
         for i in range(self.k):
-            j = np.expand_dims(self.data, axis=1) - self.mu[i]
+            j = np.expand_dims(self.X, axis=1) - self.mu[i]
             s = np.matmul(j.transpose([0, 2, 1]), j)
-            self.sigma[i] = np.matmul(s.transpose(1, 2, 0), self.z[:, i] )
+            self.sigma[i] = np.matmul(s.transpose(1, 2, 0), self.z[:, i])
             self.sigma[i] /= sum_z[i]
             
     def log_likelihood(self, X):
@@ -48,13 +39,42 @@ class GMM():
             ll.append(np.log(tot))
         return np.sum(ll)
     
-    def train(self, X):
-        self.init_em(X)
+    def train(self):
         num_iters = 60
-        log_likelihood = [self.log_likelihood(X)]
+        log_likelihood = [self.log_likelihood(self.X)]
         for e in range(num_iters):
             self.e_step()
             self.m_step()
-            log_likelihood.append(self.log_likelihood(X))
+            log_likelihood.append(self.log_likelihood(self.X))
 
-        return self.mu, self.log_likelihood(X)
+        return self.mu, -self.log_likelihood(self.X), self.accuracy()
+    
+    def euclidean(self, point, centroids):
+        return np.sqrt(np.sum((point - centroids)**2, axis=1))
+    
+    def accuracy(self):
+        distribution_means = [[-1, -1], [1, -1], [0, 1]]
+        labels = ['a', 'b', 'c']
+        label_dict = {}
+
+        for i in range(len(self.mu)):
+            center = self.mu[i]
+            dist = self.euclidean(center, distribution_means)
+            label_idx = np.argmin(dist)
+            label_dict[i] = labels[label_idx]
+
+        correct = 0
+        
+        for i in range(len(self.X)):
+            x = self.X[i]
+            label = 'a'
+            if i >= 100: label = 'b'
+            if i >= 200: label = 'c'
+
+            dist = self.euclidean(x, self.mu)
+            label_idx = np.argmin(dist)
+            prediction = label_dict[label_idx]
+
+            if label == prediction : correct += 1
+
+        return correct/len(self.X)
--- Main_GMM.py
+++ Main_GMM.py
@@ -1,11 +1,36 @@
 import numpy as np
 from GMM import GMM
+from matplotlib import pyplot as plt
+
+def plotResultsClusteringAcc(sigmas, accuracies):
+    plt.plot(sigmas, accuracies)
+    plt.xlabel('Sigma')
+    plt.ylabel('Clustering Accuracy')
+    plt.savefig('output/clusterAccGMM.png')
+    plt.show()
+
+def plotResultsClusteringObj(sigmas, objectives):
+    plt.plot(sigmas, objectives)
+    plt.xlabel('Sigma')
+    plt.ylabel('Clustering Objective')
+    plt.savefig('output/clusterObjGMM.png')
+    plt.show()
 
 def main():
-    X_train = np.loadtxt(f'data/dataset_1.txt', delimiter=' ', dtype=float)
-    gmm_model = GMM()
-    means, ll = gmm_model.train(X_train)
-    print(means, ll)
+    sigmas = [0.5, 1, 2, 4, 8]
+    results = []
+    objectives = []
+    accuracies = []
+    for sigma in sigmas:
+        X_train = np.loadtxt(f'data/dataset_{sigma}.txt', delimiter=' ', dtype=float)
+        gmm_model = GMM(X_train)
+        means, objective, accuracy = gmm_model.train()
+        objectives.append(objective)
+        accuracies.append(accuracy)
+        results.append(f'Sigma = {sigma}, Cluster Centers = {means}, Clustering Objective = {objective}, Clustering Accuracy = {accuracy}')
+    np.savetxt('Results GMM.txt', results, delimiter=' ', fmt='%s')
+    plotResultsClusteringAcc(sigmas, accuracies)
+    plotResultsClusteringObj(sigmas, objectives)
 
 if __name__ == "__main__":
     main()
