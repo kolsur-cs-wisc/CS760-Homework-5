2d8b5cf71208be931adb8f5f39517259e2863e97
Aditya Kolsur <kolsur@wisc.edu>
2023-11-07 19:15:23 -0600

    Plot K Means Results

--- Main_KMeans.py
+++ Main_KMeans.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from KMeans import KMeans
 
 sigmas = [0.5, 1, 2, 4, 8]
@@ -15,8 +16,30 @@ def generate_datasets():
             np.savetxt(f, curr_sample, delimiter = ' ')
             f.close()
 
+def plotResultsClusteringAcc():
+    x = [0.5, 1, 2, 4, 8]
+    y = [0.82, 0.68, 0.56, 0.5766666666666667, 0.4266666666666667]
+
+    plt.plot(x, y)
+    plt.xlabel('Sigma')
+    plt.ylabel('Clustering Accuracy')
+    plt.savefig('output/clusterAccKMeans.png')
+    plt.show()
+
+def plotResultsClusteringObj():
+    x = [0.5, 1, 2, 4, 8]
+    y = [326.14828206716464, 534.630041141039, 1069.8016111324475, 1470.8925872982986, 3584.8766593149785]
+
+    plt.plot(x, y)
+    plt.xlabel('Sigma')
+    plt.ylabel('Clustering Objective')
+    plt.savefig('output/clusterObjKMeans.png')
+    plt.show()
+
 def main():
     # generate_datasets()
+    plotResultsClusteringAcc()
+    plotResultsClusteringObj()
     results = []
     for sigma in sigmas:
         X_train = np.loadtxt(f'data/dataset_{sigma}.txt', delimiter=' ', dtype=float)
